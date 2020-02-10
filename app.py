b186650e560c804f91b70e4443302f8afd6fb276
mvdelaluz <56464357+mvdelaluz@users.noreply.github.com>
2020-02-09 20:27:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-import os
 from flask import Flask
-app =Flask(_name_)
+application = Flask(__name__)
 
-@app.route(“/“)
-def main():
-  return “Welcome!”
+@application.route("/")
+def hello():
+    return "Hello World!"
 
-#@app.route(‘/how are you’)
-#def hello():
-  #return ‘I am good, how about you?’
-
-#If _name_ == “_main_”:
-# app.run(host=“0.0.0.0”, port=8080)
+if __name__ == "__main__":
+    application.run()
